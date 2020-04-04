23f55715d6b4fc3e62be5566bbab4c2feeae0200
Aleks2904 <Garmatin171@yandex.ru>
2020-04-05 06:53:43 +0700

    добавлен 3ий блок

--- vebinar/app/js/swiper-active.js
+++ vebinar/app/js/swiper-active.js
@@ -37,7 +37,51 @@ $(document).ready(function () {
 
         1500:{
           slidesPerView: 5,
-          spaceBetween: 40
+          spaceBetween: 20
+        }
+      }
+
+    })
+
+    var mySwiper = new Swiper ('.swiper-container-b3', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      updateOnWindowResize: true,
+
+      spaceBetween: 30,
+
+      slidesPerView: 1,
+
+      breakpoints:{
+        320:{
+          slidesPerView: 1,
+        },
+
+        600:{
+          slidesPerView: 2,
+          spaceBetween: 60
+        },
+
+        950:{
+          slidesPerView: 3,
+          spaceBetween: 60
+        },
+
+        1500:{
+          slidesPerView: 5,
+          spaceBetween: 20
         }
       }
 
