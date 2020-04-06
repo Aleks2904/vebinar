56675af58f67353e530e4d983e3a71f026b1b0d6
Aleks2904 <Garmatin171@yandex.ru>
2020-04-06 16:02:26 +0700

    добавлен 4 блок

--- vebinar/app/js/swiper-active.js
+++ vebinar/app/js/swiper-active.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
       // Optional parameters
       direction: 'horizontal',
       loop: true,
+      slideToClickedSlide: true,
       pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
@@ -54,7 +55,11 @@ $(document).ready(function () {
 
     })
 
-    var mySwiper = new Swiper ('.b3-swiper-container', {
+   /* $('.swiper-slide').on('click', function(){
+      slideToClickedSlide: true;
+    })*/
+
+    var mySwiper2 = new Swiper ('.b3-swiper-container', {
       // Optional parameters
       direction: 'horizontal',
       loop: true,
@@ -67,6 +72,72 @@ $(document).ready(function () {
       navigation: {
         nextEl: '.b3-swiper-button-next',
         prevEl: '.b3-swiper-button-prev',
+      },
+
+      updateOnWindowResize: true,
+
+      spaceBetween: 30,
+
+      slidesPerView: 1,
+
+      breakpoints:{
+        320:{
+          slidesPerView: 1,
+        },
+
+        600:{
+          slidesPerView: 2,
+          spaceBetween: 60
+        },
+
+        950:{
+          slidesPerView: 3,
+          slidesPerColumn: 2,
+          spaceBetween: 60
+        },
+
+        1500:{
+          slidesPerView: 4,
+          slidesPerColumn: 2,
+          spaceBetween: 30
+        }
+      }
+
+    })
+
+    var menu = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    var mySwiper3 = new Swiper ('.b4-swiper-container', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      pagination: {
+        el: '.wrap__pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (menu[index]) + '</span>';
+        }
+      },
+
+      updateOnWindowResize: true,
+
+      spaceBetween: 30,
+
+      slidesPerView: 1,
+    })
+
+    var mySwiper4 = new Swiper ('.b4-swiper-container-slide', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+
+      navigation: {
+        nextEl: '.swiper-slid__next',
+        prevEl: '.swiper-slid__prev',
       },
 
       updateOnWindowResize: true,
